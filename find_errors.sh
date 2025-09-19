d69cb330859f3f3a1a35e2d444cb88eb0a8caa75
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2025-09-19 23:00:57 +0600

    logger read script done

---
+++ find_errors.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+file="./logger.log"
+
+if [ -f $file ]; then
+	echo "File exit working reast of the work"
+else 
+	echo "File not exit. Terminatting process"
+fi
+
+
+echo "Searching Full Coulmn Error using grep"
+grep -i "error" $file
+echo "-------------------******----------------"
+
+echo "Print only two column using awk command"
+
+awk '/ERROR/ { print $1, $4 }' $file
+
+
+
+
